7fed855d61a87e13fd0615a612023e3922e81319
Redwanul-Hassan-Labib <web.redwanlabib@gmail.com>
2025-07-06 22:24:59 +0600

    new api set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+  remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "157.230.240.97",
+      port: "9999",
+      pathname: "**",
+    },
+    {
+      protocol: "http",
+      hostname: "157.230.240.97",
+      pathname: "**",
+    },
+  ],
+}
+
+};
 
 export default nextConfig;
